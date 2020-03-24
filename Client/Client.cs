5eefaaa392c1cf1964dfe24acfe00d6f82152172
shelmets <anikolaev98@icloud.com>
2020-03-24 12:16:27 +0300

    Client

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -6,7 +9,22 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string ip = "192.168.1.72";
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(ip), 58900);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipe);
+            if (!socket.Connected)
+            {
+                Console.WriteLine("Fail - Connection failed");
+                return;
+            }
+            Console.WriteLine("Connected!");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.WriteLine(str);
+                socket.Send(Encoding.ASCII.GetBytes(str), Encoding.ASCII.GetBytes(str).Length, 0);
+            }
         }
     }
 }
