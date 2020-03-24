4052b080761eb5b53cecd8e09f6056ff38826337
shelmets <anikolaev98@icloud.com>
2020-03-24 12:20:12 +0300

    Server commit

--- Server/Server.cs
+++ Server/Server.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class Program
+    class Server
     {
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
         static void Main(string[] args)
         {
             IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 58900);
